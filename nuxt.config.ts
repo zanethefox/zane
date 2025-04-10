e1f3574e195e93ddefe8a0a97a71950c0fa0a924
zanethefox <62026364+zanethefox@users.noreply.github.com>
2025-04-10 22:46:08 +0200

    cf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-04-10',
   devtools: { enabled: true },
   nitro: {
+    preset: 'cloudflare',
     prerender: {
       autoSubfolderIndex: false
     }
