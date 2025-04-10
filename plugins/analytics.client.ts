1f883b0312a702382b98a3dd1e666614b172354a
zanethefox <62026364+zanethefox@users.noreply.github.com>
2025-04-10 02:18:12 +0200

    Add cookie banner

---
+++ plugins/analytics.client.ts
@@ -0,0 +1,20 @@
+import { useConsent } from '~/composables/useConsent';
+
+export default defineNuxtPlugin(() => {
+  const { hasConsent } = useConsent();
+
+  if (hasConsent('analytics')) {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1GLTD6RSWK';
+    script.async = true;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any[]) {
+      window.dataLayer.push(args);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'G-1GLTD6RSWK');
+  }
+});
